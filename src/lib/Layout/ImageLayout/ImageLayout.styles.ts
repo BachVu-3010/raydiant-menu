855eeb7c7bbd5e9a5b3ea789f8719558d2abd1d3
VinhVu <vinh.vu@eastagile.com>
2021-10-14 18:59:10 +0700

    [b] fix border between image and menu content flickers

--- src/lib/Layout/ImageLayout/ImageLayout.styles.ts
+++ src/lib/Layout/ImageLayout/ImageLayout.styles.ts
@@ -5,29 +5,13 @@ import animations, { Animation } from '../../utils/animation';
 // For the panning animations: # of px to pan from the center
 const animationOffset = 50;
 export const Image: StyledComponent<{ animation?: Animation, animate?: boolean, src: string }> = styled('div')(({ theme, animation, animate, src }) => {
-  let left = 0;
-  let right = 0;
-  let top = 0;
-  let bottom = 0;
-  if (animation === 'horizontal') {
-    left = -animationOffset;
-    right = -animationOffset;
-  }
-  if (animation === 'vertical') {
-    top = -animationOffset;
-    bottom = -animationOffset;
-  }
   return {
-    position: 'absolute',
-    top: theme.vw(top),
-    right: theme.vw(right),
-    bottom: theme.vw(bottom),
-    left: theme.vw(left),
+    width: '100%',
+    height: '100%',
     backgroundImage: `url("${src}")`,
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
-    backgroundPosition: '50% 50%',
-    objectFit: 'cover',
+    backgroundPsition: 'center center',
     animation:
       animation &&
       animations[animation]({
@@ -45,6 +29,5 @@ export const ImageLayout: StyledComponent<{ height: number, width: number }> = s
   width: theme.vw(width),
   display: 'flex',
   position: 'relative',
-  overflow: 'hidden',
   justifyContent: 'center',
 }));
